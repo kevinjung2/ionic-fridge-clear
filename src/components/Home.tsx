9f93cf0b1e384a18d878dda8429d53bd6132f153
Kevin Jung <kev.jung.ii@gmail.com>
2021-11-16 16:26:06 -0600

    adds structure to add and remove ingredient inputs

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonPage } from '@ionic/react';
+import React, { useState } from 'react';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonPage, IonGrid, IonRow, IonCol, IonItem, IonButton } from '@ionic/react';
+
+import IngredientInput from './IngredientInput'
 
 const Home: React.FC = () => {
+  const [ingredientNumber, setIngredientNumber] = useState(3)
+
   return(
     <IonPage>
       <IonHeader>
@@ -10,7 +14,20 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <h2>Hello World</h2>
+        <IonGrid>
+          <IonRow>
+            {/*This makes an array with ingredientNumber elements and maps them into inputs so the number of inputs can easily be changed with the buttons*/}
+            {[...Array(ingredientNumber).keys()].map(ingredient => <IngredientInput />)}
+          </IonRow>
+          <IonRow>
+            <IonCol className="ion-text-right">
+              <IonButton>More Ingredients</IonButton>
+            </IonCol>
+            <IonCol className="ion-text-left">
+              <IonButton>Less Ingredients</IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   )
---
+++ src/components/IngredientInput.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { IonItem } from '@ionic/react';
+
+const IngredientInput: React.FC = () => {
+  return(
+    <IonItem>
+    </IonItem>
+  )
+}
+
+export default IngredientInput
